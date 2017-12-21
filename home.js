39e526e3aeb2cccc23beb342adbc15a8507ba579
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2017-12-21 15:33:07 +0100

    small change

--- home.js
+++ home.js
@@ -28,12 +28,12 @@
         var properties = context.document.properties;
         $('#wordProps').html("Hello!");
         context.load(properties);
-        var custom = properties.customProperties;
-        context.load(custom);
-        var HAT = custom.load();
-        HAT.getCount();
-        HAT.add("CAR","MICRA");
-         $('#wordProps').html(HAT.getItem("CAR"));
+        //var custom = properties.customProperties;
+        //context.load(custom);
+        //var HAT = custom.load();
+        //HAT.getCount();
+        //HAT.add("CAR","MICRA");
+         //$('#wordProps').html(HAT.getItem("CAR"));
           return context.sync().then(function(){
             properties.title = properties.title + " Additional Title Text"; // once the sync goes off, this works.
         return context.sync();
