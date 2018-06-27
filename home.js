8e1f9e24299efc534c3630630ea96b74e7531192
Isaac Gravestock <Isaac.Gravestock@usz.ch>
2018-06-27 16:27:30 +0200

    a new little add function

--- home.js
+++ home.js
@@ -64,6 +64,17 @@
     }
   
   
+  function addProp(name, value){
+     Word.run(function(context){
+        var properties = context.document.properties;
+        context.load(properties);
+        var custom = properties.customProperties;
+        context.load(custom);
+        custom.add(name, value);
+        return context.sync();
+    });
+    
+  }//end addProp
   
     function insertfieldxmlname() {
 
@@ -155,78 +166,6 @@
     
     
 
-    
  
-    function insertEmersonQuoteAtSelection() {
-        Word.run(function (context) {
  
-            // Create a proxy object for the document.
-            var thisDocument = context.document;
- 
-            // Queue a command to get the current selection.
-            // Create a proxy range object for the selection.
-            var range = thisDocument.getSelection();
- 
-            // Queue a command to replace the selected text.
-            range.insertText('"Hitch your wagon to a star."\n', Word.InsertLocation.replace);
- 
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Ralph Waldo Emerson.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
- 
-    function insertChekhovQuoteAtTheBeginning() {
-        Word.run(function (context) {
- 
-            // Create a proxy object for the document body.
-            var body = context.document.body;
- 
-            // Queue a command to insert text at the start of the document body.
-            body.insertText('"Knowledge is of no value unless you put it into practice."\n', Word.InsertLocation.start);
- 
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from Anton Chekhov.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
- 
-    function insertChineseProverbAtTheEnd() {
-        Word.run(function (context) {
- 
-            // Create a proxy object for the document body.
-            var body = context.document.body;
- 
-            // Queue a command to insert text at the end of the document body.
-            body.insertText('"To know the road ahead, ask those coming back."\n', Word.InsertLocation.end);
- 
-            // Synchronize the document state by executing the queued commands,
-            // and return a promise to indicate task completion.
-            return context.sync().then(function () {
-                console.log('Added a quote from a Chinese proverb.');
-            });
-        })
-        .catch(function (error) {
-            console.log('Error: ' + JSON.stringify(error));
-            if (error instanceof OfficeExtension.Error) {
-                console.log('Debug info: ' + JSON.stringify(error.debugInfo));
-            }
-        });
-    }
 })();
